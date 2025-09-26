12c24dd5d0a7d36f14d1dd452650e737528b2321
mshumphrey <ehumphrey@nightingale.org>
2025-09-26 10:51:24 -0400

    Add comment; test commit

--- printX.java
+++ printX.java
@@ -1,5 +1,6 @@
-public class PrintX {
+public class printX {
     public static void main(String args[]){
+        // declare int xSize
         int xSize = 8;
         for(int i = 0; i < xSize; i++){
             for(int j = 0; j < xSize; j++){
